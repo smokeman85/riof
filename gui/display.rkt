cb974dfd9eae321ee1f44200db59c32e2e19d0e1
Dmitry Pichugin <smokeman85@gmail.com>
2017-05-27 18:34:09 +0300

    gui/display.rkt: Add using point module, some refactoring

--- gui/display.rkt
+++ gui/display.rkt
@@ -1,5 +1,7 @@
 #lang racket/gui
 
+(require "../core/point.rkt")
+
 (require "../core/image.rkt")
 (define image (read-image "../example/sample.jpg"))
 
@@ -13,34 +15,41 @@
 
 (define bitmap-canvas%
   (class canvas%
+
     (inherit get-dc)
     (super-new)
 
     ;; start point for rectangle
-    (init-field [old-x 0] [old-y 0])
+    (init-field [p0 (point 0 0)]
+                [r (rect 0 0 0 0)])
+
+    (define/private (update-end-point x y)
+      (set-field! r this (build-rect p0 (point x y))))
 
     (define/private (draw-rectangle x y)
-      (define old-x (get-field old-x this))
-      (define old-y (get-field old-y this))
+      (define old-x (point-x (get-field p0 this)))
+      (define old-y (point-y (get-field p0 this)))
       (define w (abs (- old-x x)))
       (define h (abs (- old-y y)))
       (send (get-dc) draw-rectangle old-x old-y w h))
 
     (define/override (on-event event)
-      (send msg set-label (format "x:~a, y:~a" (send event get-x) (send event get-y)))
+      (define x (send event get-x))
+      (define y (send event get-y))
+      (send msg set-label (format "x:~a, y:~a" x y))
       (cond
         [(and (send event button-changed? 'left) (send event button-down? 'left))
-         (set-field! old-x this (send event get-x))
-         (set-field! old-y this (send event get-y))]
+         (set-field! p0 this (point x y))]
         [(and (send event button-changed? 'left) (send event button-up? 'left))
          (send this on-paint)
          (send (get-dc) set-brush "red" 'transparent)
-         (draw-rectangle (send event get-x) (send event get-y))]))))
+         (draw-rectangle x y)]))))
     
 
 (define canvas (new bitmap-canvas% [parent frame]
-             [paint-callback
-              (lambda (canvas dc)
-                (send dc draw-bitmap image 0 0))]))
+                    [paint-callback
+                     (lambda (canvas dc)
+                       (send dc draw-bitmap image 0 0))]))
+
 (send frame show #t)
 
