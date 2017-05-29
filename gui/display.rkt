0175ae5da2e8d5755aa56739dd8953bdcaa83690
Dmitry Pichugin <smokeman85@gmail.com>
2017-05-29 18:41:20 +0300

    gui/display.rkt: Get rect from showing image

--- gui/display.rkt
+++ gui/display.rkt
@@ -2,14 +2,6 @@
 
 (require "../core/point.rkt")
 
-(require "../core/image.rkt")
-(define image (read-image "../example/sample.jpg"))
-
-(define frame (new frame%
-                   [label "Bitmap"]
-                   [width (send image get-width)]
-                   [height (send image get-height)]))
-
 (define bitmap-canvas%
   (class canvas%
 
@@ -20,32 +12,55 @@
     (init-field [p0 (point 0 0)]
                 [r (rect 0 0 0 0)])
 
+    ;; Get rect
+    (define/public (get-rect)
+      r)
+
+    ;; Build rect by end point
     (define/private (update-end-point x y)
       (set-field! r this (build-rect p0 (point x y))))
 
+    ;; Draw rectangle with end x and y of image
     (define/private (draw-rectangle x y)
       (define old-x (point-x (get-field p0 this)))
       (define old-y (point-y (get-field p0 this)))
       (define w (abs (- old-x x)))
       (define h (abs (- old-y y)))
+      (update-end-point x y)
       (send (get-dc) draw-rectangle old-x old-y w h))
 
     (define/override (on-event event)
       (define x (send event get-x))
       (define y (send event get-y))
       (cond
+        ;; First mouse left click and pressed
         [(and (send event button-changed? 'left) (send event button-down? 'left))
          (set-field! p0 this (point x y))]
+        ;; Left mouse button is up
         [(and (send event button-changed? 'left) (send event button-up? 'left))
          (send this on-paint)
          (send (get-dc) set-brush "red" 'transparent)
          (draw-rectangle x y)]))))
-    
 
-(define canvas (new bitmap-canvas% [parent frame]
-                    [paint-callback
-                     (lambda (canvas dc)
-                       (send dc draw-bitmap image 0 0))]))
+(define (get-image-rect image)
+  (define lock-thread (thread (lambda () (let loop ()
+                                           (loop)))))
+  (define frame (new frame%
+                     [label "Bitmap"]
+                     [width (send image get-width)]
+                     [height (send image get-height)]))
+  (define canvas (new bitmap-canvas% [parent frame]
+                      [paint-callback
+                       (lambda (canvas dc)
+                         (send dc draw-bitmap image 0 0))]))
+  (new button% [parent frame]
+       [label "Get rect"]
+       [callback (lambda (button event)
+                   (send frame show #f)
+                   (kill-thread lock-thread))])
+  (send frame show #t)
+  (yield (thread-dead-evt lock-thread))
+  (send canvas get-rect))
 
-(send frame show #t)
-
+(provide (contract-out
+           [get-image-rect (-> object? rect?)]))
