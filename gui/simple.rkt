f2f373201e8f5cbab95be8d65ce28f6af1e51459
Dmitry Pichugin <smokeman85@gmail.com>
2017-05-29 15:16:15 +0300

    gui/simple.rkt: Draft of locking GUI thread

--- gui/simple.rkt
+++ gui/simple.rkt
@@ -7,5 +7,26 @@
   (new message% [parent f] [label image])
   (send f show #t))
 
+(define (show-bitmap-lock image #:label [label "Bitmap"])
+  (define lock-thread (thread (lambda ()
+                         (let loop ()
+                           (sleep 0.2)
+                           (loop)))))
+  (define f (new frame% [label label]))
+  (new message% [parent f] [label image])
+  (new button% [parent f]
+       [label "Exit"]
+             [callback (lambda (button event)
+                         (send f show #f)
+                         (kill-thread lock-thread))])
+  (send f show #t)
+  (yield (thread-dead-evt lock-thread)))
+
 (provide (contract-out
           [show-bitmap (->* (object?) (#:label string?) any)]))
+
+
+(require "../core/image.rkt")
+(define image (read-image "../example/sample.jpg"))
+(show-bitmap-lock image)
+(displayln "End")
