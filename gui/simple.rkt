7ec12de1f2fa6c7480c859b2422ca14180687248
Dmitry Pichugin <smokeman85@gmail.com>
2017-05-29 17:59:40 +0300

    gui/simple.rkt: Update show-bitmap-lock and create contract for it

--- gui/simple.rkt
+++ gui/simple.rkt
@@ -7,26 +7,22 @@
   (new message% [parent f] [label image])
   (send f show #t))
 
+;; show-bitmap : bitmap? [string?] -> frame
+;; Simple view of bitmap with label and lock main thread
 (define (show-bitmap-lock image #:label [label "Bitmap"])
-  (define lock-thread (thread (lambda ()
-                         (let loop ()
-                           (sleep 0.2)
-                           (loop)))))
+  (define lock-thread (thread (lambda () (let loop ()
+                                           (loop)))))
   (define f (new frame% [label label]))
   (new message% [parent f] [label image])
   (new button% [parent f]
-       [label "Exit"]
-             [callback (lambda (button event)
-                         (send f show #f)
-                         (kill-thread lock-thread))])
+       [label "Continue..."]
+       [callback (lambda (button event)
+                   (send f show #f)
+                   (kill-thread lock-thread))])
   (send f show #t)
   (yield (thread-dead-evt lock-thread)))
 
 (provide (contract-out
-          [show-bitmap (->* (object?) (#:label string?) any)]))
+          [show-bitmap (->* (object?) (#:label string?) any)]
+          [show-bitmap-lock (->* (object?) (#:label string?) any)]))
 
-
-(require "../core/image.rkt")
-(define image (read-image "../example/sample.jpg"))
-(show-bitmap-lock image)
-(displayln "End")
