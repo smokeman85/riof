e4ee0ad601999a3b365d22b1e1406f9ccc7e6f6c
Dmitry Pichugin <smokeman85@gmail.com>
2017-06-09 09:16:22 +0300

    core/affine.rkt: Update rotate function

--- core/affine.rkt
+++ core/affine.rkt
@@ -2,16 +2,19 @@
 
 (require racket/draw)
 
-(define (rotate-image image angle)
+(define (rotate-image image angle #:x [x 0] #:y [y 0])
   (define w (send image get-width))
   (define h (send image get-height))
   (define bitmap-blank (make-bitmap w h))
+  (define dx (/ w 2))
+  (define dy (/ h 2))
   (define dc (make-object bitmap-dc% bitmap-blank))
-  (send dc set-smoothing 'aligned)
-  (send dc translate 0 h)
+  (define t (send dc get-transformation))
+  (send dc set-smoothing 'smoothed)
+  (send dc set-origin (+ x dx) (+ y dy))
   (send dc rotate angle)
-  (send dc draw-bitmap image 0 0)
-  (send dc get-bitmap)
+  (send dc draw-bitmap image (- dx) (- dy))
+  (send dc set-transformation t)
   (or (send dc get-bitmap) (bitmap-blank)))
 
 ; Test
@@ -21,6 +24,6 @@
 (define image (read-image "../example/sample.jpg"))
 
 (show-bitmap image)
-(define rimage (rotate-image image (degrees->radians 90)))
+(define rimage (rotate-image image (degrees->radians 60)))
 (show-bitmap rimage)
 
