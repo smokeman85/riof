b5ee2f58f5696642dcbc4cd25dd421e4137e569f
Dmitry Pichugin <smokeman85@gmail.com>
2017-06-10 13:57:30 +0300

    core/affine.rkt: Add scale function

--- core/affine.rkt
+++ core/affine.rkt
@@ -17,6 +17,21 @@
   (send dc set-transformation t)
   (or (send dc get-bitmap) (bitmap-blank)))
 
+(define (scale-image image scale-x scale-y #:x [x 0] #:y [y 0])
+  (define w (send image get-width))
+  (define h (send image get-height))
+  (define bitmap-blank (make-bitmap w h))
+  (define dx (/ w 2))
+  (define dy (/ h 2))
+  (define dc (make-object bitmap-dc% bitmap-blank))
+  (define t (send dc get-transformation))
+  (send dc set-smoothing 'smoothed)
+  (send dc set-origin (+ x dx) (+ y dy))
+  (send dc scale scale-x scale-y)
+  (send dc draw-bitmap image (- dx) (- dy))
+  (send dc set-transformation t)
+  (or (send dc get-bitmap) (bitmap-blank)))
+
 ; Test
 (require "image.rkt")
 (require "../gui/simple.rkt")
@@ -24,6 +39,6 @@
 (define image (read-image "../example/sample.jpg"))
 
 (show-bitmap image)
-(define rimage (rotate-image image (degrees->radians 60)))
+(define rimage (scale-image image 2 2))
 (show-bitmap rimage)
 
